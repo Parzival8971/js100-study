c55ba6ca9e8dbc97691f3d465c568d9439fbb9e4
Parzival8971 <esh8971@gmail.com>
2022-08-30 00:56:26 +0900

    3번과 4번 문제를 추가한다

---
+++ 1-10/03.js
@@ -0,0 +1,13 @@
+// Q 다음 출력 값으로 올바른 것은?
+var arr = [100, 200, 300];
+console.log(typeof(arr));
+// 1)  undefined
+// 2)  string
+// 3)  number
+// 4)  object
+
+// Answer
+//Omuing_ver1 
+undefined, string, number 는 모두 기본 자료형(primitive type) 입니다.
+
+// sjshin_ver1
