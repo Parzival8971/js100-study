7e800b9d66f0e39558eca3ba9cc7babf9e1692ed
Soojung Shin <doublesj@Soojungui-MacBook-Pro.local>
2022-08-30 00:33:10 +0900

    codekata day1

--- 1-10/01.js
+++ 1-10/01.js
@@ -4,9 +4,22 @@ var nums = [100, 200, 300, 400, 500];
 // Answer 0
 var newArray = nums.slice(0, 3);
 console.log(newArray);
-console.log('구분선')
+console.log('구분선');
 
 // Answer 2
 nums.pop();
 nums.pop();
 console.log(nums);
+
+//sjshin_ver1
+nums.pop();
+nums.pop();
+console.log(nums);
+
+//sjshin_ver2
+const nums2 = nums.slice(0, 3);
+console.log(nums2);
+
+//sjshin_ver3
+const nums3 = nums.splice(0, 3);
+console.log(nums3);
