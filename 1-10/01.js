8d5c7d3c69bb8497e697846f3121469e14ad6c9a
Parzival8971 <esh8971@gmail.com>
2022-08-30 00:40:06 +0900

    conflict를 해결한다

--- 1-10/01.js
+++ 1-10/01.js
@@ -1,12 +1,12 @@
-// Q 배열에서 400, 500를 삭제하는 code를 입력하세요.
-var nums = [100, 200, 300, 400, 500];
+// Q 배열에서 400, 500를 삭제하는 code를 입력하라
+const nums = [100, 200, 300, 400, 500];
 
 // Answer 0
-var newArray = nums.slice(0, 3);
+const newArray = nums.slice(0, 3);
 console.log(newArray);
 console.log('구분선')
 
-// Answer 2
+// Answer 1
 nums.pop();
 nums.pop();
 console.log(nums);
