dc49e20f65d13a39cdcd38ebf43f122def92ca48
Parzival8971 <esh8971@gmail.com>
2022-09-01 00:43:35 +0900

    11번 문제를 푼다

---
+++ 1-10/11.js
@@ -0,0 +1,16 @@
+// Q 1부터 100까지 모두 더하는 Code를 <pass> 부분에 완성하세요. for를 사용해야 합니다.
+let s = 0;
+
+//pass
+console.log(s);
+
+
+// Answer
+//Omuing_ver1 => for문에 익숙해지자!!
+let total = 0;
+for(let i=1; i<=100; i++) {
+	total = total + i;
+}
+console.log(total);
+
+//sjshin
