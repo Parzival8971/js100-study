141edb3bd41442cae20227f751d14dc37ccc7d32
Parzival8971 <esh8971@gmail.com>
2022-08-31 22:47:21 +0900

    8번 문제를 푼다

---
+++ 1-10/08.js
@@ -0,0 +1,18 @@
+// Q 자바스크립트 객체를 다음과 같이 만들었다. 
+//출력값을 입력하시오. (출력값은 공백을 넣지 않습니다. )
+
+var d = {
+  'height':180,
+  'weight':78,
+  'weight':84,
+  'temperature':36,
+  'eyesight':1,
+  example:24
+};
+
+// Answer
+//Omuing_ver1
+console.log(d['weight']); // []대괄호 접근은 ''를 써줘야 함. 이 부분 좀 헷갈림
+console.log(d[example]); // 왼쪽 처럼 접근불가능 ''로 감싸줘야함. 저것은 변수명 접근임
+
+//sjshin
