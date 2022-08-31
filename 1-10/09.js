9ad85c0f3da3a174a4cacd135c51c64f6c3b5197
Parzival8971 <esh8971@gmail.com>
2022-08-31 23:33:27 +0900

    9번 문제를 푼다

---
+++ 1-10/09.js
@@ -0,0 +1,20 @@
+// Q 다음 소스 코드를 완성하여 날짜와 시간을 출력하시오.
+var year = '2019';
+var month = '04';
+var day = '26';
+var hour = '11';
+var minute = '34';
+var second = '27';
+
+var result = //빈칸을 채워주세요
+console.log(result);
+
+출력
+2019/04/26 11:34:27
+
+// Answer
+//Omuing_ver1
+var result = year.concat('/', month, '/', day, ' ', hour, ':', minute, ':', second);
+// 허허... 그냥 이렇게 때려버린다구? 쉽고 아주 좋아!! 캬컄
+
+//sjshin
