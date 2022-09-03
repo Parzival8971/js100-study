8553d7bfe97b10a59260e1bc641ebbbd2f05e2fa
Parzival8971 <esh8971@gmail.com>
2022-09-03 18:36:54 +0900

    19번 문제를 푼다

---
+++ 11-20/19.js
@@ -0,0 +1,10 @@
+// Q 제곱을 구하자
+// 공백으로 구분하여 두 숫자 a와 b가 주어지면, a의 b승을 구하는 프로그램을 작성하세요.
+
+// Answer
+//Omuing 0
+const num = ["2", "6"]
+console.log(Math.pow(parseInt(num[0],10),parseInt(num[1],10)))
+//Omuing 1
+
+//sjshin
