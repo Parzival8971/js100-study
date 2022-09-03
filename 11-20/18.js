ab97dffa08c91738bc9a2ba3bc136f834ea510b1
Parzival8971 <esh8971@gmail.com>
2022-09-03 18:09:49 +0900

    18번 문제를 푼다

---
+++ 11-20/18.js
@@ -0,0 +1,22 @@
+// Q 영하네 반은 국어, 수학, 영어 시험을 보았습니다. 영하는 친구들의 평균 점수를 구해주기로 했습니다.
+// 공백으로 구분하여 세 과목의 점수가 주어지면 전체 평균 점수를 구하는 프로그램을 작성하세요. 
+// 단, 소숫점 자리는 모두 버립니다.
+
+// Answer
+//Omuing 0
+const scores = ["70", "82", "95"]
+let sum = 0;
+for(let i=0; i<scores.length; i++) {
+  sum = sum + parseInt(scores[i], 10);
+}
+console.log(Math.floor(sum/scores.length));
+//Omuinge 답안
+const scores = prompt().split(' ')
+console.log(scores)
+let sum = 0;
+for(let i=0; i<scores.length; i++) {
+  sum = sum + parseInt(scores[i], 10);
+}
+console.log(Math.floor(sum/scores.length));
+
+//sjshin
