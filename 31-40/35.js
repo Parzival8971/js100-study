206dff0d2f6c4c262c77999cd283a4c93dd4da06
Parzival8971 <esh8971@gmail.com>
2022-09-06 00:36:41 +0900

    36번 문제를 푼다

--- 31-40/35.js
+++ 31-40/35.js
@@ -24,10 +24,10 @@ function one(n){
   }
   return two;
 }
-const a = one(2);
-const b = one(3);
-const c = one(4);
-console.log(a(10));
-console.log(b(10));
-console.log(c(10));
+const as = one(2);
+const bs = one(3);
+const cs = one(4);
+console.log(as(10));
+console.log(bs(10));
+console.log(cs(10));
 //sjshin
