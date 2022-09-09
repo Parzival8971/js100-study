e9c5ebefd6cb6f26b95cda4c0a1fc4fb17416a81
Parzival8971 <esh8971@gmail.com>
2022-09-09 17:48:54 +0900

    48번 문제를 푼다

---
+++ 41-50/48.js
@@ -0,0 +1,19 @@
+// Q 대소문자 바꿔서 출력하기
+// 문자열이 주어지면 대문자와 소문자를 바꿔서 출력하는 프로그램을 작성하세요.
+
+// 입출력
+// 입력 : AAABBBcccddd
+// 출력 : aaabbbCCCDDD// Answer
+
+//Omuing
+const string = "AAABBBcccddd";
+let arrs = [];
+for(let i=0; i<string.length; i++) {
+  if(string[i] === string[i].toUpperCase()){
+    arrs.push(string[i].toLowerCase());
+  } else {
+    arrs.push(string[i].toUpperCase());
+  }
+}
+console.log(arrs.join(""))
+//sjshin
