361fbfef5476a574cbff3ce9f1bbc96a8f506056
Parzival8971 <esh8971@gmail.com>
2022-09-16 15:30:49 +0900

    1번 문제를 2회 푼다

--- 1-10/01.js
+++ 1-10/01.js
@@ -1,12 +1,12 @@
 // Q 배열에서 400, 500를 삭제하는 code를 입력하라
 const nums = [100, 200, 300, 400, 500];
 
-// Answer 0
-const newArray = nums.slice(0, 3);
+// Oum 1 - 2회독 중
+const newArray = nums.slice(0, 3); // 기존배열은 보존, 새로운 배열을 반환함, 인덱스가 0부터 3까지 복사(3은 제외)
+console.log(nums.splice(0, 3)); // 기존 배열을 변형시킴
 console.log(newArray);
-console.log('구분선');
 
-// Answer 1
+// Oum 2
 nums.pop();
 nums.pop();
 console.log(nums);
@@ -28,4 +28,4 @@ console.log(nums); //[ 400, 500 ]
 
 //🌸 sePark 🌸
 console.log(nums.slice(0, 3)); //원본 배열에 영향을 주지 않는다
-console.log(nums.splice(0, 3));// 원본 대열의 값도 변하고, splice한 값도 변수에 지정하여 반환 받을 수 있다
+console.log(nums.splice(0, 3)); // 원본 대열의 값도 변하고, splice한 값도 변수에 지정하여 반환 받을 수 있다
