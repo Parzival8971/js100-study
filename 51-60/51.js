60239805d20339e93b063e4f3c59bb705a1cc004
Parzival8971 <esh8971@gmail.com>
2022-09-17 21:00:35 +0900

    51,52번 문제를 푼다

--- 51-60/51.js
+++ 51-60/51.js
@@ -37,7 +37,7 @@ const array = [1,3,5,4,8,6,7,2]
 console.log(mergeSort(array));
 
 // Answer
-//// 🙈 Oum => 1회독 이해못함;;;
+//// 🙈 Oum 1회독 => 풀려면 재귀함수에 대해 알아야함. 아직 못품
 function mergeSort(arr){
   if (arr.length <= 1){
     return arr;
--- 51-60/52.js
+++ 51-60/52.js
@@ -20,8 +20,27 @@ const array = prompt('배열을 입력하세요').split(' ').map(n => parseInt(n
 console.log(quickSort(array));
 
 // Answer
-//// 🙈 Oum 1회독
+//// 🙈 Oum 1회독 => 풀려면 재귀함수에 대해 알아야함. 아직 못품
+const quickSort = (arr) => {
+	if(arr.length <= 1 ){
+		return arr
+  }
+  const pivot = arr[0];
+  const left = [];
+  const right = [];
+  
+  for(let i=1; i<arr.length; i++) {
+    if(arr[i] < pivot) {
+      left.push(arr[i])
+    } else {
+      right.push(arr[i])
+    }
+  }
+  return quickSort(left).concat(pivot, quickSort(right))
+}
 
+const array = [1,3,2,7,5,6,4,8,9,10]
+console.log(quickSort(array));
 //sjshin
 
 // 🌸 sePark 🌸
